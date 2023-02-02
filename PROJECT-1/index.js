a52d6ad5d7a4d916c9080378fec6bf40dce74cfa
KawulJan <aytoldi.uii@gmail.com>
2023-02-02 22:53:30 +0900

    small Projects

---
+++ PROJECT-1/index.js
@@ -0,0 +1,17 @@
+const btnEl = document.getElementById("btn");
+const apiKey = {
+
+}
+
+const options = {
+    method: "GET",
+    headers: {
+
+    }
+}
+
+function getJoke() {
+    console.log("clicked");
+}
+
+btnEl.addEventListener("click", getJoke);
---
+++ project-2/scrpit.js
@@ -0,0 +1,7 @@
+const ratingEls = document.querySelectALL("rating");
+
+ratingEls.forEach((ratingEls) => {
+    ratingEls.addEventListener("click", (event) => {
+        
+    })
+});
